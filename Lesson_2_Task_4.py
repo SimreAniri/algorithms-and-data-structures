f975d64125c082b0e022b69fabcfb20fce6acfa1
SimreAniri <simre_aniri@bk.ru>
2019-05-29 15:42:46 +0300

    Оптимизация алгоритма.

--- Lesson_2_Task_4.py
+++ Lesson_2_Task_4.py
@@ -4,15 +4,10 @@
 
 n = int(input('Введите количество чисел в ряде (натуральное число): '))
 n_i = 1
-summa = 1
+summa = 0
 
-for i in range(n-1):
-    n_i /= 2
-
-    if i % 2 == 0:
-        summa -= n_i
-
-    else:
-        summa += n_i
+for i in range(n):
+    summa += n_i
+    n_i /= -2
 
 print(f'Сумма чисел последовательности равна: {summa}')
