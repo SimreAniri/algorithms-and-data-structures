e7d8442283ee80bb7ac0a877b0edea4143be643d
SimreAniri <simre_aniri@bk.ru>
2019-05-31 13:37:00 +0300

    Реализован поиск максимального из минимальных элементов в массиве.

---
+++ Lesson_3_Task_5.py
@@ -0,0 +1,34 @@
+# В массиве найти максимальный отрицательный элемент.
+# Вывести на экран его значение и позицию в массиве.
+# Примечание к задаче: пожалуйста не путайте «минимальный» и «максимальный отрицательный».
+# Это два абсолютно разных значения.
+# Если таких элементов несколько, то вывожу любой из них.
+
+
+import random
+
+
+SIZE = 10
+MIN_ITEM = -20
+MAX_ITEM = 50
+array = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
+print('Массив:', array, sep='\n')
+
+max_negative_num = 0
+num_index = 0
+
+for i, item in enumerate(array):
+
+    if item < 0 and max_negative_num == 0:
+        max_negative_num = item
+        num_index = i
+
+    elif max_negative_num < item < 0:
+        max_negative_num = item
+        num_index = i
+
+if max_negative_num == 0:
+    print('Отрицательных элементов не найдено.')
+
+else:
+    print(f'Максимальный отрицательный элемент: {max_negative_num}\nЕго индекс: {num_index}')
