57ae65e262cf9091a6f42e4589065d1278d05240
SimreAniri <simre_aniri@bk.ru>
2019-06-10 03:17:00 +0300

    Реализован алгоритм поиска i-ого простого числа. Произведена оценка сложности алгоритма.

--- Lesson_4_Task_2.py
+++ Lesson_4_Task_2.py
@@ -9,33 +9,32 @@
 
 import cProfile
 
-# def test(num, n=10000):
-#     sieve = [i for i in range(n)]
-#     sieve[1] = 0
-#
-#     for i in range(2, n):
-#         if sieve[i] != 0:
-#             j = i + i
-#             while j < n:
-#                 sieve[j] = 0
-#                 j += i
-#
-#     res = [i for i in sieve if i != 0]
-#     print(f'Количество простых чисел в диапазоне до {n}: {len(res)}')
-#
-#     assert num < len(res)
-#     return res[num - 1]
+
+def test(num, n=10000):
+    sieve = [i for i in range(n)]
+    sieve[1] = 0
+
+    for i in range(2, n):
+        if sieve[i] != 0:
+            j = i + i
+            while j < n:
+                sieve[j] = 0
+                j += i
+
+    res = [i for i in sieve if i != 0]
+    print(f'Количество простых чисел в диапазоне до {n}: {len(res)}')
+
+    assert num < len(res)
+    return res[num - 1]
 
 
 def eratosthenes_sieve(n):
-    count = 0
-    start = 0
-    end = 3 * n
+    count = 1
+    start = 3
+    end = 4 * n
 
-    sieve = [i for i in range(start, end)]
-    sieve[1] = 0
-
-    prime = []
+    sieve = [i for i in range(start, end) if i % 2 != 0]
+    prime = [2]
 
     while count < n:
 
@@ -51,12 +50,12 @@ def eratosthenes_sieve(n):
 
                 while j < len(sieve):
                     sieve[j] = 0
-                    j += i
+                    j += sieve[i]
 
         prime.extend([i for i in sieve if i != 0])
 
-        start, end = end, end + 1 * n
-        sieve = [i for i in range(start, end)]
+        start, end = end, end + 2 * n
+        sieve = [i for i in range(start, end) if i % 2 != 0]
 
         for i in range(len(sieve)):
             for num in prime:
@@ -65,31 +64,77 @@ def eratosthenes_sieve(n):
 
 # py -m timeit -n 100 -s "import Lesson_4_Task_2" "Lesson_4_Task_2.eratosthenes_sieve(10)"
 # "Lesson_4_Task_2.eratosthenes_sieve(10)"
-# 100 loops, best of 5: 5.49 usec per loop
+# 100 loops, best of 5: 4.69 usec per loop
 # "Lesson_4_Task_2.eratosthenes_sieve(100)"
-# 100 loops, best of 5: 1.51 msec per loop
+# 100 loops, best of 5: 517 usec per loop
 # "Lesson_4_Task_2.eratosthenes_sieve(1000)"
-# 100 loops, best of 5: 215 msec per loop
+# 100 loops, best of 5: 82.2 msec per loop
 # Предположительно, алгоритм сложности O(n**2). Увеличение количества чисел в 10 раз
-# увеличивает время выполнения более, чем в 100 раз
+# увеличивает время выполнения приблизительно в 100 раз
 
-# cProfile.run('eratosthenes_sieve(10)')
-# 1    0.000    0.000    0.000    0.000 Lesson_4_Task_2.py:30(eratosthenes_sieve)
-# 1    0.000    0.000    0.000    0.000 Lesson_4_Task_2.py:35(<listcomp>)
-# cProfile.run('eratosthenes_sieve(100)')
-# 1    0.002    0.002    0.002    0.002 Lesson_4_Task_2.py:30(eratosthenes_sieve)
-# 1    0.000    0.000    0.000    0.000 Lesson_4_Task_2.py:35(<listcomp>)
-# 3    0.000    0.000    0.000    0.000 Lesson_4_Task_2.py:56(<listcomp>)
-# 3    0.000    0.000    0.000    0.000 Lesson_4_Task_2.py:59(<listcomp>)
 # cProfile.run('eratosthenes_sieve(1000)')
-# 1    0.203    0.203    0.203    0.203 Lesson_4_Task_2.py:30(eratosthenes_sieve)
-# 1    0.000    0.000    0.000    0.000 Lesson_4_Task_2.py:35(<listcomp>)
-# 5    0.000    0.000    0.000    0.000 Lesson_4_Task_2.py:56(<listcomp>)
-# 5    0.000    0.000    0.000    0.000 Lesson_4_Task_2.py:59(<listcomp>)
+# 1    0.000    0.000    0.000    0.000 Lesson_4_Task_2.py:31(eratosthenes_sieve)
+# 1    0.000    0.000    0.000    0.000 Lesson_4_Task_2.py:36(<listcomp>)
+# cProfile.run('eratosthenes_sieve(100)')
+# 1    0.001    0.001    0.001    0.001 Lesson_4_Task_2.py:31(eratosthenes_sieve)
+# 1    0.000    0.000    0.000    0.000 Lesson_4_Task_2.py:36(<listcomp>)
+# 1    0.000    0.000    0.000    0.000 Lesson_4_Task_2.py:55(<listcomp>)
+# 1    0.000    0.000    0.000    0.000 Lesson_4_Task_2.py:58(<listcomp>)
+# cProfile.run('eratosthenes_sieve(1000)')
+# 1    0.104    0.104    0.105    0.105 Lesson_4_Task_2.py:31(eratosthenes_sieve)
+# 1    0.000    0.000    0.000    0.000 Lesson_4_Task_2.py:36(<listcomp>)
+# 2    0.000    0.000    0.000    0.000 Lesson_4_Task_2.py:55(<listcomp>)
+# 2    0.000    0.000    0.000    0.000 Lesson_4_Task_2.py:58(<listcomp>)
 # Время выполнения нарастает. Рекурсий нет.
 
 
-# n = 1000
+def search_prime(n):
+    count = 0
+    number = 1
+    prime = []
+
+    while count != n:
+        number += 1
+
+        for num in prime:
+            if number % num == 0:
+                break
+        else:
+            count += 1
+            prime.append(number)
+
+    return number
+
+# py -m timeit -n 100 -s "import Lesson_4_Task_2" "Lesson_4_Task_2.search_prime(10)"
+# "Lesson_4_Task_2.search_prime(10)"
+# 100 loops, best of 5: 4.58 usec per loop
+# "Lesson_4_Task_2.search_prime(100)"
+# 100 loops, best of 5: 216 usec per loop
+# "Lesson_4_Task_2.search_prime(1000)"
+# 100 loops, best of 5: 18.9 msec per loop
+# Алгоритм быстрее, чем eratosthenes_sieve(). Сложность близка к O(n**2)
+
+# cProfile.run('search_prime(1000)')
+# 1    0.000    0.000    0.000    0.000 Lesson_4_Task_2.py:91(search_prime) 10
+# 1    0.000    0.000    0.000    0.000 Lesson_4_Task_2.py:91(search_prime) 100
+# 1    0.023    0.023    0.023    0.023 Lesson_4_Task_2.py:91(search_prime) 1000
+# Время выполнения нарастает. Рекурсий нет.
+
+
+# ВЫВОД:
+# Сложность алгоритмов приблизительно одинаковая, но алгоритм search_prime() работает быстрее.
+
+
+n = 521
+
 # if eratosthenes_sieve(n) == test(n):
 #     print(f'{n}-ое простое число {eratosthenes_sieve(n)}')
 #     print('OK')
+# else:
+#     print('Ошибка')
+
+# if search_prime(n) == test(n):
+#     print(f'{n}-ое простое число {search_prime(n)}')
+#     print('OK')
+# else:
+#     print('Ошибка')
