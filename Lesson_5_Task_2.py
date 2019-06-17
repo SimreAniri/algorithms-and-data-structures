a5c8427a32d64c755d1ae823f38d813bf50bdfb8
SimreAniri <simre_aniri@bk.ru>
2019-06-17 15:34:34 +0300

    Реализована функция умножения шестнадцатиричных чисел.

--- Lesson_5_Task_2.py
+++ Lesson_5_Task_2.py
@@ -21,9 +21,9 @@ def sum_hex(x, y):
     else:
         x, y = deque(x), deque(y)
 
-    while len(x):
+    while x:
 
-        if len(y):
+        if y:
             res = hex_num[x.pop()] + hex_num[y.pop()] + transfer
 
         else:
@@ -50,10 +50,39 @@ def mult_hex(x, y):
                0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9,
                10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'}
     result = deque()
-    spam = [0 for _ in range(len(y))]
+    spam = deque([deque() for _ in range(len(y))])
+
+    x, y = x.copy(), deque(y)
 
     for i in range(len(y)):
-        pass
+        m = hex_num[y.pop()]
+
+        for j in range(len(x) - 1, -1, -1):
+            spam[i].appendleft(m * hex_num[x[j]])
+
+        for _ in range(i):
+            spam[i].append(0)
+
+    transfer = 0
+
+    for _ in range(len(spam[-1])):
+        res = transfer
+
+        for i in range(len(spam)):
+            if spam[i]:
+                res += spam[i].pop()
+
+        if res < 16:
+            result.appendleft(hex_num[res])
+
+        else:
+            result.appendleft(hex_num[res % 16])
+            transfer = res // 16
+
+    if transfer:
+            result.appendleft(hex_num[transfer])
+
+    return result
 
 
 print('Для ввода шестнадцатиричных чисел используйте:')
@@ -64,4 +93,4 @@ b = list(input('Введите 2-е шестнадцатиричное числ�
 
 print(*a, '+', *b, '=', *sum_hex(a, b))
 
-print(*a, '*', *b, '=', *sum_hex(a, b))
+print(*a, '*', *b, '=', *mult_hex(a, b))
